2bc5bceb33f2a07e0d24769260b231c4ecd395e5
ajochymczyk <ajochymczykagh@gmail.com>
2024-05-04 14:45:50 +0200

    front for future display (needs changes)

---
+++ app/AMMScript.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./style.css";
+
+export const AmmScript = () => {
+  return (
+    <div className="AMM-script">
+      <div className="div">
+        <div className="overlap">
+          <button className="home-button">
+            <div className="overlap-group">
+              <div className="text-wrapper">Home</div>
+            </div>
+          </button>
+          <div className="about-button">
+            <div className="div-wrapper">
+              <div className="text-wrapper-2">About</div>
+            </div>
+          </div>
+          <div className="info-button">
+            <div className="overlap-2">
+              <div className="text-wrapper-3">Info</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-3">
+          <div className="background">
+            <div className="overlap-4">
+              <div className="rectangle" />
+              <div className="rectangle-2" />
+              <img className="line" alt="Line" src="https://c.animaapp.com/a0hzXRiM/img/line-.svg" />
+              <div className="text-wrapper-4">Write your code below:</div>
+            </div>
+          </div>
+          <div className="compile-button">
+            <div className="overlap-5">
+              <div className="text-wrapper-5">compile</div>
+            </div>
+          </div>
+        </div>
+        <div className="text-wrapper-6">© 2024 AMM Script</div>
+        <div className="text-wrapper-7">AMM Script</div>
+      </div>
+    </div>
+  );
+};
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AmmScript } from '../app/AMMScript.js'; 
+import '../app/style.css'; 
+
+const HomePage = () => {
+  return (
+    <div>
+      <AmmScript /> 
+    </div>
+  );
+};
+
+export default HomePage;

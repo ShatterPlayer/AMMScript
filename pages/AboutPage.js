54ef2e406ec86a674953d6563f26c65a6c0c653d
ajochymczyk <ajochymczykagh@gmail.com>
2024-05-05 15:03:27 +0200

    Added hovers new colour

--- pages/AboutPage.js
+++ pages/AboutPage.js
@@ -8,7 +8,7 @@ export const aboutPage = () => {
   return (
     <div className="About-page">
       <div className="div">
-        <Navbar/>
+       <Navbar/>
         <div className="overlap-3">
           <div className="background">
             <div className="overlap-4">
@@ -17,6 +17,19 @@ export const aboutPage = () => {
               <div className="code-input"/>
               <img className="line" alt="Line" src="https://c.animaapp.com/a0hzXRiM/img/line-.svg" />
               <div className="text-wrapper-4">Główne założenia:</div>
+              <div className="text-wrapper-8">
+              <div className="text-wrapper-8">
+                <div>1. Prostota składni - AMM Script został zaprojektowany w sposób umożliwiający intuicyjną naukę dla początkujących programistów.</div>
+                <div>2. Zmienne - Język obsługuje zmienne, które mogą przechowywać różne typy danych. W przykładzie stosujemy set dla przypisania wartości do zmiennych: set x 10</div>
+                <div>set y “Hello World!”</div>
+                <div>Wypisanie:</div>
+                <div>print x</div>
+                <div>print y</div>
+                <div>3. Obsługa błędów - na przykład obsługa błędów dla przypadku kiedy użytkownik próbuje odwołać się do niezidentyfikowanej zmiennej</div>
+                <div>4. Modularność - użytkownicy będą mogli dodawać nowe funkcje, instrukcje lub typy danych, żeby język był lepiej przystosowany do różnych zastosowań</div>
+                <div>5. Czytelność - kod łatwy do zrozumienia i analizowania</div>
+              </div>
+              </div>
             </div>
           </div>
           <a href="#" class="more-info-button">
